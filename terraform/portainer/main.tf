b156d0ce1443cc9a893143f58661b7b7f314e91b
s3lcsum <dreewniak@gmail.com>
2025-05-22 00:46:17 +0200

    feat(portainer): consolidate stack management and add OAuth2 support

--- terraform/portainer/main.tf
+++ terraform/portainer/main.tf
@@ -20,3 +20,59 @@ provider "portainer" {
   api_key         = var.portainer_api_key
   skip_ssl_verify = true
 }
+
+resource "portainer_settings" "default" {
+  authentication_method = 3
+  enable_telemetry      = true
+
+  dynamic "oauth_settings" {
+    for_each = var.portainer_oauth2_enabled ? [1] : []
+
+
+    content {
+      access_token_uri        = var.portainer_oauth2_access_token_uri
+      authorization_uri       = var.portainer_oauth2_authorization_uri
+      client_id               = var.portainer_oauth2_client_id
+      client_secret           = var.portainer_oauth2_client_secret
+      logout_uri              = var.portainer_oauth2_logout_uri
+      redirect_uri            = var.portainer_oauth2_redirect_uri
+      oauth_auto_create_users = true
+    }
+  }
+}
+
+locals {
+  portainer_stacks = [
+    "authentik",
+    "cloudflared",
+    "cups",
+    "mktxp",
+    "netbootxyz",
+    "nginx_proxy_manager",
+    "upsnap",
+    "uptime_kuma",
+    "victoria",
+    # "watchtower",
+    "watchyourlan",
+  ]
+}
+
+resource "portainer_stack" "stacks" {
+  for_each = toset(local.portainer_stacks)
+
+  deployment_type = "standalone"
+  method          = "string"
+
+  endpoint_id = var.endpoint_id
+
+  name               = each.value
+  stack_file_content = file("../../stacks/${each.value}/compose.yaml")
+
+  dynamic "env" {
+    for_each = try(var.portainer_stacks_envs[each.value], {})
+    content {
+      name  = env.key
+      value = env.value
+    }
+  }
+}
--- terraform/portainer/variables.tf
+++ terraform/portainer/variables.tf
@@ -14,7 +14,43 @@ variable "endpoint_id" {
   type        = number
 }
 
+variable "portainer_oauth2_enabled" {
+  description = "Enable OAuth2 authentication"
+  type        = bool
+  default     = false
+}
+
 variable "portainer_stacks_envs" {
   description = "Environment variables for the stacks"
   type        = map(map(string))
+}
+
+variable "portainer_oauth2_access_token_uri" {
+  description = "Portainer OAuth2 access token URI"
+  type        = string
+}
+
+variable "portainer_oauth2_authorization_uri" {
+  description = "Portainer OAuth2 authorization URI"
+  type        = string
+}
+
+variable "portainer_oauth2_client_id" {
+  description = "Portainer OAuth2 client ID"
+  type        = string
+}
+
+variable "portainer_oauth2_client_secret" {
+  description = "Portainer OAuth2 client secret"
+  type        = string
+}
+
+variable "portainer_oauth2_logout_uri" {
+  description = "Portainer OAuth2 logout URI"
+  type        = string
+}
+
+variable "portainer_oauth2_redirect_uri" {
+  description = "Portainer OAuth2 redirect URI"
+  type        = string
 }
